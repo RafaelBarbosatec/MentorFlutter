4c8e27fec53c2745d0e4abe1428da760389508b3
Teógenes Teles <teocteles@gmail.com>
2019-06-01 13:58:35 -0300

    Home

---
+++ lib/pages/home/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaerialApp(
+      title: 'Home',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Minha Home'),
+        ),
+      )
+      
+    );
+  }
+}
