2cabc35a237558f64e6662b7e52cff6073a4c82c
Teógenes Teles <teocteles@gmail.com>
2019-06-01 14:09:40 -0300

    homes

---
+++ lib/pages/home/home_screen_nivel_1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeNivelUm extends StatefulWidget {
+  @override
+  _HomeNivelUmState createState() => _HomeNivelUmState();
+}
+
+class _HomeNivelUmState extends State<HomeNivelUm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Minha Home MENOS EXPERIENTE'),
+        ),
+      );
+  }
+}
---
+++ lib/pages/home/home_screen_nivel_2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeNivelDois extends StatefulWidget {
+  @override
+  _HomeNivelDoisState createState() => _HomeNivelDoisState();
+}
+
+class _HomeNivelDoisState extends State<HomeNivelDois> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Minha Home MAIS EXPERIENTE'),
+        ),
+      );
+  }
+}
